b7b7eb834fa5ee48f4044de4e728e49c46ec7a08
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-01-24 14:04:05 +0200

    2

--- js/modal.js
+++ js/modal.js
@@ -1,12 +1,20 @@
 (() => {
   const refs = {
-    openModalBtn: document.querySelector('[data-modal-open]'),
+    openModalBtn: document.querySelectorAll('[data-modal-open]'),
     closeModalBtn: document.querySelector('[data-modal-close]'),
     modal: document.querySelector('[data-modal]'),
   };
 
-  refs.openModalBtn.addEventListener('click', toggleModal);
   refs.closeModalBtn.addEventListener('click', toggleModal);
+  for (let btnList of refs.openModalBtn) {
+    btnList.addEventListener('click', toggleModal);
+ function toggleModal() {
+    document.body.classList.toggle('modal-open');
+    refs.modal.classList.toggle('is-hidden');
+  }
+
+  }
+  
 
   function toggleModal() {
     document.body.classList.toggle('modal-open');
