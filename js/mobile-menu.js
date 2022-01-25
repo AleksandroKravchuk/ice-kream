978075d22e9d130ec5fd8c53a2b6987ea4c03a85
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-01-25 09:48:37 +0200

    1

--- js/mobile-menu.js
+++ js/mobile-menu.js
@@ -6,22 +6,14 @@
 
   
   menuBtnRef.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
+ 
 
     menuBtnRef.classList.toggle("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
- 
     mobileMenuRef.classList.toggle("is-open");
     document.body.classList.toggle("modal-open");
   });
   menuBtnClose.addEventListener("click", () => {
-    const expanded =
-      menuBtnRef.getAttribute("aria-expanded") === "true" || false;
-
     menuBtnRef.classList.remove("is-open");
-    menuBtnRef.setAttribute("aria-expanded", !expanded);
- 
     mobileMenuRef.classList.remove("is-open");
     document.body.classList.remove("modal-open");
   });
