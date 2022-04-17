a219be1df016e8f3320302441a55025ab62232f0
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-04-17 22:27:31 +0300

    доробляю вікно заказа

--- js/change window.js
+++ js/change window.js
@@ -1,2 +1,27 @@
-// const formMain = document.querySelectorAll('.js-active');
-// console.log(formMain)
+const formMain = document.querySelector('.js-window');
+const formSecondary = document.querySelector('.js-changewindow');
+const newClient = document.querySelector('#new-client');
+const oldClient = document.querySelector('#old-client');
+
+
+console.log(formMain);
+console.log(newClient);
+console.log(oldClient);
+
+newClient.addEventListener('click', changeWindow);
+
+function changeWindow(event) {
+  formMain.classList.remove('change-hidden');
+    formSecondary.classList.add('change-hidden');
+     newClient.classList.add('act');
+    oldClient.classList.remove('act');
+}
+
+oldClient.addEventListener('click', oxystUpWindow);
+
+function oxystUpWindow() {
+    formSecondary.classList.remove('change-hidden');
+    formMain.classList.add('change-hidden');
+    newClient.classList.remove('act');
+    oldClient.classList.add('act');
+}
