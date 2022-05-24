ad5355447ec947cacd1ca5f21120abf6b6ac6150
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-05-24 15:37:25 +0300

    2

--- js/qeschions.js
+++ js/qeschions.js
@@ -4,11 +4,12 @@ const refs = {
     formQeschions: document.querySelector('.questions__form'),
     listQeschions: document.querySelector('.qeschions-list'),
     btnPut: document.querySelector('#qeschions'),
-    qeschionsBlock: document.querySelector('.qeschions')
+    qeschionsBlock: document.querySelector('.qeschions'),
 }
 
 refs.formQeschions.addEventListener('submit', setQeschion);
 refs.btnPut.addEventListener('click', renderList);
+refs.listQeschions.addEventListener('click',onDelete)
 
 let items = [];
 
@@ -40,8 +41,14 @@ function renderList(evt) {
     fetchMessage().then((data) => data.map(({id, message }) => { 
     const render =       
  `
-<li >
- <p><span>${id}</span>.  ${message}</p>
+<li class="qeschion-item >
+<div class="qeschion-section">
+ <div class="qeschion"><p><span>${id}</span>.  ${message}</p> </div>
+ <div class="answer"> <input class="answer-text" type="text-area"></div>
+ <button data-id="${id}" class="qeschion-close" type="button">X</bottom>
+ 
+</div>
+ 
 </li>`;
         refs.listQeschions.insertAdjacentHTML('beforeend', render);   
       
@@ -49,5 +56,36 @@ function renderList(evt) {
     
 }
 
-// const deleteMessage = ()=>axios.delete("https://6283bdb838279cef71dbbcec.mockapi.io/api/v1/qeschions/7");
+
+  
+function onDelete(evt) {
+    
+    const BTN = evt.target.closest('button');
+    if (!BTN) return;
+    const { id } = BTN.dataset;
+   
+   const deleteMessage = ()=> axios.delete(`https://6283bdb838279cef71dbbcec.mockapi.io/api/v1/qeschions/${id}`);
+    deleteMessage();
+
+   refs.listQeschions.innerHTML = '';
+clear(id)
+}
+function clear(idn) {
+ return   fetchMessage().then((data) => data.map(({ id, message }) => { 
+     if (id !== idn) {
+         const render =       
+ `
+<li class="qeschion-item >
+<div class="qeschion-section">
+ <div class="qeschion"><p><span>${id}</span>.  ${message}</p> </div>
+ <div class="answer"> <input class="answer-text" type="text-area"></div>
+ <button data-id="${id}" class="qeschion-close" type="button">X</bottom>
+</div>
+</li>`;
+        refs.listQeschions.insertAdjacentHTML('beforeend', render);  }
+   
+      
+    }));
+} 
+// const deleteMessage = ()=>axios.delete("https://6283bdb838279cef71dbbcec.mockapi.io/api/v1/qeschions/4");
 // deleteMessage()
