f297c24d5975202c15019783649aeb37f3498312
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-06-03 13:11:20 +0300

    2

---
+++ js/galerry.js
@@ -0,0 +1,50 @@
+// import SimpleLightbox from "simplelightbox";
+// const gallery = new SimpleLightbox('.gallery a');
+
+// console.log(gallery)
+const imgContainer = document.querySelector('.our-products__list');
+console.log(imgContainer);
+const galleryItems = [
+  {
+    preview:
+      'src="./images/main/main.jpg" ',
+    original:
+      'href="./images/main/main2x.jpg"',
+    description: 'ICE CREAM',
+  },
+  {
+    preview:
+      'src="./images/card/card1.jpg"',
+    original:
+      'href="./images/card/card1x2.jpg"',
+    description: 'CAYENNE CHOCOLATE',
+  },
+  {
+    preview:
+      'src="./images/card/card2.jpg"',
+    original:
+      'href="./images/card/card2x2.jpg"',
+    description: 'CAKE BATTER',
+  },
+  {
+    preview:
+      'src="./images/card/card3.jpg"',
+    original:
+      'href="./images/card/card3x2.jpg"',
+    description: 'CANDY CANE',
+  },
+  {
+    preview:
+      'src="./images/card/card4.jpg"',
+    original:
+      'href="./images/card/card4x2.jpg"',
+    description: 'PLATTERS',
+  },
+  {
+    preview:
+      'src="./images/card/card1.jpg"',
+    original:
+      'href="./images/card/card1x2.jpg"',
+    description: 'DESSERT',
+  },
+];
