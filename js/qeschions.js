39f6e8e8232d8964ed7465639c76db19cf1c07a6
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-06-03 10:43:50 +0300

    modal qeschion

--- js/qeschions.js
+++ js/qeschions.js
@@ -52,7 +52,7 @@ const fetchMessage = () =>
  `
 <li class="qeschion-item >
 <div class="qeschion-section">
- <div class="qeschion"><p class"qeschion-text"><span>${id}</span >.  ${message}</p> </div>
+ <div class="qeschion"><p class"qeschion-text" > <span>${id}</span >. ${message}</p> </div>
  <div class="answer"> <input class="answer-text"  data-id="${id}"  type="text" name="text"><p class="answer-span">${answer}</p></div>
  <button data-id="${id}" class="qeschion-close" type="button">X</bottom>
  
