a784966858a49626889471900b12843598d9c415
Aleksandr Kravchuk <alexandrokrk@gmail.com>
2022-06-03 14:06:56 +0300

    1

--- js/galerry.js
+++ js/galerry.js
@@ -1,50 +1,88 @@
-// import SimpleLightbox from "simplelightbox";
-// const gallery = new SimpleLightbox('.gallery a');
-
-// console.log(gallery)
-const imgContainer = document.querySelector('.our-products__list');
-console.log(imgContainer);
 const galleryItems = [
   {
     preview:
-      'src="./images/main/main.jpg" ',
+      "./images/main/main.jpg" ,
     original:
-      'href="./images/main/main2x.jpg"',
+      "./images/main/main2x.jpg",
     description: 'ICE CREAM',
   },
   {
     preview:
-      'src="./images/card/card1.jpg"',
+      "./images/card/card1.jpg",
     original:
-      'href="./images/card/card1x2.jpg"',
+      "./images/card/card1x2.jpg",
     description: 'CAYENNE CHOCOLATE',
   },
   {
     preview:
-      'src="./images/card/card2.jpg"',
+      "./images/card/card2.jpg",
     original:
-      'href="./images/card/card2x2.jpg"',
+      "./images/card/card2x2.jpg",
     description: 'CAKE BATTER',
   },
   {
     preview:
-      'src="./images/card/card3.jpg"',
+     "./images/card/card3.jpg",
     original:
-      'href="./images/card/card3x2.jpg"',
+      "./images/card/card3x2.jpg",
     description: 'CANDY CANE',
   },
   {
     preview:
-      'src="./images/card/card4.jpg"',
+      "./images/card/card4.jpg",
     original:
-      'href="./images/card/card4x2.jpg"',
+      "./images/card/card4x2.jpg",
     description: 'PLATTERS',
   },
   {
     preview:
-      'src="./images/card/card1.jpg"',
+      "./images/card/card1.jpg",
     original:
-      'href="./images/card/card1x2.jpg"',
+      "./images/card/card1x2.jpg",
     description: 'DESSERT',
   },
 ];
+
+
+const imgContainer = document.querySelector('.our-products__list');
+// const cardsMarkup = createImgCards(galleryItems);  
+// imgContainer.insertAdjacentHTML('beforeend', cardsMarkup);
+
+// function createImgCards(galleryItems) {
+//     return galleryItems.map(({ preview, original, description }) => {
+//         return `
+//   <div class="gallery__item">
+//   <a class="gallery__link" href="${original}">
+//     <img
+//       class="gallery__image"
+//       src="${preview}"
+//       data-source="${original}"
+//       alt="${description}"
+//     />
+//   </a>
+// </div>
+//     `
+//     }).join('');
+// }
+
+imgContainer.addEventListener('click', onContainerClick);
+
+let currentImg = '';
+function onContainerClick(event) {
+
+  if (!event.target.classList.contains('our-products__link')) {
+    return;
+  }
+  event.preventDefault();
+  console.log("its me")
+  // imgContainer.addEventListener('keydown', onImageClose);
+  const size = event.target.dataset.source;
+
+  currentImg = basicLightbox.create(`
+		   <img  src="${size}" >
+	`
+  );
+  currentImg.show();
+
+}
+ 
